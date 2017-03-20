131e2a0e971dd4adedb3536427a6fae3be870b28
noam finkelstein <noam.finkelstein@gmail.com>
2017-03-20 15:34:44 -0400

    Generalize warp to multiple knots

--- fwarp/warp.py
+++ fwarp/warp.py
@@ -1,31 +1,53 @@
 import matplotlib.pyplot as plt
 import numpy as np
 
-def expand_contract(func, period=1, factor=.3):
-    """ factor of .5 gives original function """
-    if factor < 0 or factor > 1:
-        raise ValueError("factor must be between 0 and 1")
+def warp(func, knots, weights, period):
+    """
+    params
+    ------
+    func: The function to be warped.
+    knots: A list of numbers between 0 and 1
+    weights: A list of length 1 greater than knots, representing how much of the
+        function should fall between each knot. Weights must sum to 1.
+    period: The absolute length at which the warps repeat
 
+    returns:
+    ------
+    A new function where the weights determine how much of the old function is
+    gotten by each of the knots.
+    """
+    if sum(weights) != 1:
+        raise ValueError("Weights must sum to 1")
+    if max(knots) >= 1 or min(knots) <= 0:
+        raise ValueError("Knots must fall between 0 and 1")
+
+    knots = [0] + knots + [1]
     def distort(x):
         num_periods = x // period
         remainder_fraction = (x % period) / period
-        if remainder_fraction <= factor:
-            new_remainder = .5 * remainder_fraction / factor
-        else:
-            new_remainder = .5 + .5 * (remainder_fraction - factor) / (1 - factor)
+
+        for i, knot in enumerate(knots):
+            if remainder_fraction <= knot:
+                break
+
+        i -= 1
+        start = sum(weights[:i])
+        proportion = (remainder_fraction - knots[i]) / (knots[i + 1] - knots[i])
+        new_remainder = start + (weights[i] * proportion)
+
         return (num_periods * period) + (new_remainder * period)
 
-    return warp(func, distort)
+    return build_warped(func, distort)
 
 def compress(func, factor=2):
     def distort(x):
         return factor * x
-    return warp(func, distort)
+    return build_warped(func, distort)
 
 def elongate(func, factor=2):
     return compress(func, factor=1/factor)
 
-def warp(func, distortion_func):
+def build_warped(func, distortion_func):
     def warped(x):
         distorted_x = np.vectorize(distortion_func)(x)
         return func(distorted_x)
