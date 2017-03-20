47f506e635e7c160c49b9bfdb2ad26bc16d9fa43
noam finkelstein <noam.finkelstein@gmail.com>
2017-03-20 13:48:26 -0400

    Bump version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='fwarp',
-    version='0.0.3',
+    version='0.0.4',
     description='Utility for warping functions',
     url='https://github.com/n-s-f/warp',
     packages=find_packages(exclude=['contrib', 'docs', 'tests', 'examples']),
