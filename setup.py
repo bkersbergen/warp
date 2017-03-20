db72ab7c5d567c2f1d90deb25ba4071732df303e
noam finkelstein <noam.finkelstein@gmail.com>
2017-03-20 12:10:56 -0400

    Better setup

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='warp',
+    version='0.0.1',
+    description='Utility for warping functions',
+    url='https://github.com/n-s-f/warp',
+    packages=find_packages(exclude=['contrib', 'docs', 'tests', 'examples']),
+    install_requires=['pywavelets', 'matplotlib'],
+)
