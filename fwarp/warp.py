6e375963f21d1748de4d80456d11f356e14f0263
noam finkelstein <noam.finkelstein@gmail.com>
2017-04-03 09:15:12 -0400

    Add starting options and improve constraints

--- fwarp/warp.py
+++ fwarp/warp.py
@@ -192,6 +192,7 @@ def find_best_bezier(func1, func2, x, knot_points, cost=None , log=False,
                      optimization_method='Powell', to_return='function',
                      optimization_options=None):
     # assumes x starts as zero
+    print("EHH")
     if cost is None:
         def cost(x, y):
             return np.abs(x.flatten() - y.flatten())**2
@@ -213,8 +214,9 @@ def find_best_bezier(func1, func2, x, knot_points, cost=None , log=False,
     starting = np.concatenate((starting_warp, starting_scale))
     optimal = sop.minimize(distance, starting, method=optimization_method,
                            options=optimization_options)
-    # assert optimal.success, 'Optimization failed'
+    assert optimal.success, 'Optimization failed'
 
+    # print(weights)
     weights = optimal.x
     if log:
         weights = np.exp(weights)
@@ -247,36 +249,49 @@ def spline_scale(func, knots, yvalues, period):
     scale = generate_spline_scale(knots, yvalues)
     return functional_scale(func, scale, period)
 
-def find_best_spline(func1, func2, x, knot_points, cost=None , log=False,
+def find_best_spline(func1, func2, x, interior_knots, cost=None , log=False,
                      optimization_method='Powell', to_return='function',
-                     optimization_options=None):
+                     optimization_options=None, starting_weights=None):
+    # interior knots does not include the two on the edges (0 and 1)
     # assumes x starts as zero
     if cost is None:
         def cost(x, y):
             return np.abs(x.flatten() - y.flatten())**2
 
-    num_knots = len(knot_points)
-    diagonal = np.linspace(0, 1, num_knots + 2)
+    knot_points = [(i + 1)/(interior_knots + 1) for i in range(interior_knots)]
+    diagonal = np.linspace(0, 1, interior_knots + 2)
     def distance(params):
         #TODO: deal with log case
         warp_params, scale_params = np.split(params, [len(knot_points)])
 
-        all_warp = np.zeros(num_knots + 2)
+        all_warp = np.zeros(interior_knots + 2)
         all_warp[1:] = np.concatenate((warp_params, np.ones(1)))
-        if (np.abs(all_warp - diagonal) > (1 / (2*(num_knots + 2)))).any() \
-            or any(fabs(sub(*a)) > .75 for a in zip(scale_params, scale_params[1:])) \
+        # conditions: warp can only be 1 / interior_knots away from diagonal
+        # (adding more knots then keeps close to diagonal - don't add too many)
+        # scale control points can only jump .5 from one to one
+        # scale must stay between .5 and 2
+        # NOTE: At the extremes allowed here with 2 interior knots, the
+        # distortion function can be induced to briefly have negative slope.
+        # Fix this be evaluating the gradient everywhere and rejecting any negatve values?
+        # (too computationally intensive?)
+        if (np.abs(all_warp - diagonal) > (1 / (2*(interior_knots + 2)))).any() \
+            or any(fabs(sub(*a)) > .5 for a in zip(scale_params, scale_params[1:])) \
             or (scale_params > 2).any() or (scale_params < 0.5).any():  
             return sys.maxsize
 
         intermediary = spline_warp(func1, knot_points, warp_params, max(x))
         new_func1 = spline_scale(intermediary, knot_points, scale_params, max(x))
         total_cost = np.sum(cost(new_func1(x), func2(x)))
-        # print(total_cost)
+        #print(total_cost)
         return total_cost
 
-    starting_warp = np.linspace(0, 1, len(knot_points) + 2)[1:-1]
-    starting_scale = np.ones(len(knot_points) + 2)
-    starting = np.concatenate((starting_warp, starting_scale))
+    if starting_weights is None:
+        starting_warp = np.linspace(0, 1, len(knot_points) + 2)[1:-1]
+        starting_scale = np.ones(len(knot_points) + 2)
+        starting = np.concatenate((starting_warp, starting_scale))
+    else:
+        starting = np.concatenate((starting_weights['warp'], starting_weights['scale']))
+       
     optimal = sop.minimize(distance, starting, method=optimization_method,
                            options=optimization_options)
     assert optimal.success, 'Optimization failed'
@@ -293,9 +308,9 @@ def find_best_spline(func1, func2, x, knot_points, cost=None , log=False,
         intermediary = spline_warp(func1, knot_points, both_weights['warp'], max(x))
         return spline_scale(intermediary, knot_points, both_weights['scale'], max(x))
 
+def find_canonical(funcs, num_interior_knots):
 
-
-
+   pass
 
 def compress(func, factor=2):
     def distort(x):
--- fwarp/warp_test.py
+++ fwarp/warp_test.py
@@ -1,22 +1,26 @@
 from .warp import *
+import cProfile
 import numpy as np
 
-def test_best_bezier():
+def test_best_spline():
     # given
     knots = [1/3, 2/3]
-    warp_yvalues = [.1, .9]
+    warp_yvalues = [.2, .8]
     scale_yvalues = [.6, 1.2, 1.7, .5]
-    warped = bezier_warp(np.sin, knots, warp_yvalues, np.pi * 2)
-    distorted = bezier_scale(warped, knots, scale_yvalues, np.pi * 2)
+    warped = spline_warp(np.sin, knots, warp_yvalues, np.pi * 2)
+    distorted = spline_scale(warped, knots, scale_yvalues, np.pi * 2)
 
     # when
-    print("about to optimize")
-    best = find_best_bezier(
-        np.sin, distorted, np.linspace(0, 2*np.pi), knots, to_return='weights',
-        optimization_options={'maxfev': 10}
+    best = find_best_spline(
+        np.sin, distorted, np.linspace(0, 2*np.pi), 2, to_return='weights',
     )
 
     # then
-    assert np.allclose(best['warp'], warp_yvalues)
-    assert np.allclose(best['scale'], scale_yvalues)
+    print(best)
+    assert np.allclose(best['warp'], warp_yvalues, atol=.1)
+    assert np.allclose(best['scale'], scale_yvalues, atol=.1)
+
+if __name__ == '__main__':
+    #cProfile.run('test_best_spline()', sort='cumulative')
+    pass
 
